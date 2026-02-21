d8bf236ff17ac3cc0ae04dd1c279f358aba819fd
na <nalebao@gmail.com>
2026-02-20 19:13:51 -0800

    Added arrange button to rearrange cards in order

--- main.py
+++ main.py
@@ -21,10 +21,23 @@ def main():
      # Distributes 13 cards from deck into user hand
     user_cards = USER_HAND(DistCards(cardsList, cardCount, deck, usedList))
 
-    x_position = 100
-    for card in user_cards.cards:
-        card.render(canvas, x_position, 500)
-        x_position += 50
+    # Draw 13 randoms cards
+    def draw_hand():
+        canvas.delete("all")
+        x_position = 100
+        for card in user_cards.cards:
+            card.render(canvas, x_position, 500)
+            x_position += 50
+
+        canvas.create_window(700,400,window=arrange_button)
+    
+    # Arrange button function
+    def arrange_card():
+        user_cards.cards.sort(key=lambda card: (card.rank.value, card.suit.SuitRank))
+        draw_hand()
+
+    arrange_button = tk.Button(root, text="Arrange", command=arrange_card)
+    draw_hand()
 
     root.mainloop()
 
