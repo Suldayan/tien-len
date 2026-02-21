c38586d7f67d354665960621fd6003af4b5b34e6
yuvrajnarula16 <yuvrajnarula16@gmail.com>
2026-02-20 18:07:16 -0800

    Added in card distribution logic along with user and cpu hands

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import random
 import tkinter as tk
+from src.hands import USER_HAND
 from src.deck import DECK
+from src.distCards import DistCards
 
 def main():
     deck = DECK()
+
+    #amount of max cards per hand
+    cardCount = 13
+    cardsList = []
+    usedList = []
 
     root = tk.Tk()
     root.title("Card Demo")
@@ -11,11 +18,11 @@ def main():
     canvas = tk.Canvas(root, width=800, height=600, bg="green")
     canvas.pack()
 
-    # Draw 13 randoms cards
-    sample_cards = random.sample(deck.cards, 13)
+     # Distributes 13 cards from deck into user hand
+    user_cards = USER_HAND(DistCards(cardsList, cardCount, deck, usedList))
 
     x_position = 100
-    for card in sample_cards:
+    for card in user_cards.cards:
         card.render(canvas, x_position, 500)
         x_position += 50
 
---
+++ src/hands.py
@@ -0,0 +1,16 @@
+class USER_HAND:
+    def __init__(self, cardsList):
+        self.cards = cardsList
+
+#Bot classes to hold cpu cards if we want 3 cpu
+class BOT1_HAND:
+    def __init__(self, cardsList):
+        self.cards = cardsList
+
+class BOT2_HAND:
+    def __init__(self, cardsList):
+        self.cards = cardsList
+
+class BOT3_HAND:
+    def __init__(self, cardsList):
+        self.cards = cardsList
